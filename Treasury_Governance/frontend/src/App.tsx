282d3763e94fcc73169231ab84f4e25e704ee10a
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2025-10-10 17:55:37 +0100

    update

---
+++ Treasury_Governance/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Homepage from './components/treasury'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen w-full">
+      <Homepage />
+    </div>
+  )
+}
+
+export default App
